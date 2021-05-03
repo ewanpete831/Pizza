7ba5fc188ffc81921b5791b9fd776ac2b0ce418c
ewanpete831 <ewanpete831@outlook.com>
2021-05-03 11:23:46 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Pizza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pizzaLabel = new System.Windows.Forms.Label();
+            this.toppingsLabel = new System.Windows.Forms.Label();
+            this.toppingsInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pizzaLabel
+            // 
+            this.pizzaLabel.BackColor = System.Drawing.Color.Red;
+            this.pizzaLabel.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pizzaLabel.Location = new System.Drawing.Point(-1, -2);
+            this.pizzaLabel.Name = "pizzaLabel";
+            this.pizzaLabel.Size = new System.Drawing.Size(688, 121);
+            this.pizzaLabel.TabIndex = 0;
+            this.pizzaLabel.Text = "Pizza Shop";
+            this.pizzaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // toppingsLabel
+            // 
+            this.toppingsLabel.AutoSize = true;
+            this.toppingsLabel.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppingsLabel.ForeColor = System.Drawing.Color.White;
+            this.toppingsLabel.Location = new System.Drawing.Point(107, 172);
+            this.toppingsLabel.Name = "toppingsLabel";
+            this.toppingsLabel.Size = new System.Drawing.Size(304, 39);
+            this.toppingsLabel.TabIndex = 1;
+            this.toppingsLabel.Text = "Number of Toppings:";
+            // 
+            // toppingsInput
+            // 
+            this.toppingsInput.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppingsInput.Location = new System.Drawing.Point(418, 184);
+            this.toppingsInput.Name = "toppingsInput";
+            this.toppingsInput.Size = new System.Drawing.Size(168, 35);
+            this.toppingsInput.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.Color.White;
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.calculateButton.FlatAppearance.BorderSize = 3;
+            this.calculateButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.calculateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(231, 235);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(223, 73);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(114, 356);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(454, 93);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(687, 519);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.toppingsInput);
+            this.Controls.Add(this.toppingsLabel);
+            this.Controls.Add(this.pizzaLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pizza";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pizzaLabel;
+        private System.Windows.Forms.Label toppingsLabel;
+        private System.Windows.Forms.TextBox toppingsInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizza
+{
+    public partial class Form1 : Form
+    {
+        //setup variables
+        double baseCost = 7.00;
+        double toppingCost = 1.25;
+        double toppingCount;
+        double totalCost;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                //get topping count from text box
+                toppingCount = Convert.ToDouble(toppingsInput.Text);
+
+                //calculate cost of Pizza with toppings
+                totalCost = baseCost + (toppingCount * toppingCost);
+
+                //display total cost
+                outputLabel.Text = $"The cost of a pizza with {toppingCount} toppings is {totalCost.ToString("C")}";
+            }
+            catch
+            {
+                //send error message
+                outputLabel.Text = $"Please enter a valid number of toppings!";
+            }
+
+
+
+        }
+    }
+}
